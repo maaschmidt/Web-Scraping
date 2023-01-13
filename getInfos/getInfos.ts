7254b997c03377ca0bacad624a7718d4c2c2f44a
Marcel Schmidt <marcel.schmidt@universo.univates.br>
2023-01-13 11:27:54 -0300

    update without solve captcha

---
+++ getInfos/getInfos.ts
@@ -0,0 +1,90 @@
+import puppeteer from "puppeteer-extra";
+import { executablePath } from "puppeteer";
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+const initBrowser = async (url: string) => {
+  puppeteer.use(StealthPlugin())
+  const browser = await puppeteer.launch({ headless: false, executablePath: executablePath() })
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36',
+  );
+
+  await page.goto(url, { waitUntil: 'load', timeout: 60000 });
+
+  // if (await page.$('#px-captcha')){
+  //   const iframe = await page.$('#px-captcha');
+  //   await iframe.evaluate((el) => el.style.display = 'flex');
+  //   await iframe.evaluate((el) => el.style.width = 20 + '%');
+    
+  //   await page.waitForTimeout(1500);
+
+  //   await page.click('#px-captcha', { button: 'right', clickCount: 3});
+    
+  //   await page.waitForNavigation({waitUntil: 'load', timeout: 600000})
+  // }
+  return { page, browser };
+}
+
+export const getUrl = async (baseURL: string) => {
+  const { page, browser } = await initBrowser(baseURL);
+
+  await page.waitForSelector('.CardVehicle__linkPhoto');
+  const urls = await page.$$eval('.sc-hwwEjo.czIXqu', elements => {
+    return elements.map(element => element.href);
+  })
+
+  await browser.close();
+
+  const reducedArray = urls.reduce((acc, curr) => {
+    if (acc.length) {
+      const lastPushed = acc[acc.length - 1];
+      if (lastPushed[0] !== curr) {
+        acc.push([curr]);
+      }
+    } else {
+      acc.push([curr]);
+    }
+    return acc;
+  }, []);
+
+  const urlsFormated = [];
+  for (const element of reducedArray) {
+    urlsFormated.push(element[0]);
+  }
+
+  return urlsFormated;
+}
+
+export const getCarInfos = async (baseURL: string) => {
+  const { page, browser } = await initBrowser(baseURL);
+
+  await page.waitForNetworkIdle();
+  const name = await page.$eval('.VehicleDetails__header__title', el => el.textContent.replace('\n', ' '));
+  const value = await page.$eval('.Forms__vehicleSendProposal__container__price', el => el.textContent);
+  const fipe = await page.$eval('.VehicleDetailsFipe__price.VehicleDetailsFipe__price--fipe', el => el.textContent.replace(/[a-zA-QS-Z]+/g, '').replace(/\s/g, '').replace('$', '$ '));
+  const grossDetails = await page.$$eval('.VehicleDetails__list__item', elements => {
+    return elements.map(element => element.innerText.replace('\n', ": "))
+  });
+
+  await browser.close();
+
+  let details: any = {};
+  let itens: any = []; 
+  grossDetails.map(detail => {
+    if(detail){
+      let splited = detail.split(': ');
+      if(splited[1]){
+        splited[0] = splited[0].normalize('NFD').replace(/\s/g, '').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+        details[splited[0]] = splited[1]
+      } else if (splited[0] != 'undefined'){
+        itens.push(splited[0])
+      }
+    }
+  });
+  if (itens){
+    details.itens = itens;
+  }
+  
+  return({ name, value, fipe, details });
+}
