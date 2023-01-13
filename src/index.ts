301733bc56d3e31e774d147721ff78f7ebbc1c8b
Marcel Schmidt <marcel.schmidt@universo.univates.br>
2023-01-13 11:27:39 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { getPagesInfos } from "../getPagesInfos";
+import { getInfos } from "../getInfos";
 
-const baseURL = 'https://www.olx.com.br/imoveis/aluguel/estado-rs/regioes-de-porto-alegre-torres-e-santa-cruz-do-sul/outras-cidades?gsp=1&sd=4354&sd=4310';
+const baseURL = "https://www.webmotors.com.br/carros/estoque/de.2010?tipoveiculo=carros&anode=2010&media=com%20fotos&precoate=35000&precode=20000&cambio=Autom%C3%A1tica%7CManual&opcionais=Ar%20condicionado&combustivel=Gasolina%7CGasolina%20e%20%C3%A1lcool";
 
 (async () => {
-  const pages = await getPagesInfos.getUrl(baseURL);
+  // const pages = await getInfos.getUrl(baseURL)
 
-  for (let i = 0; i < pages.length; i++) {
-    const infos = await getPagesInfos.getHomeInfo(pages[i])
-    console.log(infos);
-  }
-  
+  // for (let i = 0; i < pages.length; i++) {  
+    const infos = await getInfos.getCarInfos('https://www.webmotors.com.br/comprar/nissan/march/10-s-16v-flex-4p-manual/4-portas/2012-2013/43187226?pos=a43187226g:&np=1')
+    console.log(infos)
+  // }
+
 })()
